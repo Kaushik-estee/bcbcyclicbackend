20025eba1827c6110e5cb5e14bafa62ad713b33a
kaushik.estee <kaushik.estee@gmail.com>
2023-09-27 16:13:13 +0530

    kk

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const cors = require('cors');
+
+
+const { connection } = require("./db")
+const { carRouter } = require("./routes/car.routes")
+
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors());
+
+app.use("/car",carRouter)
+
+app.get("/",(req,res)=>{
+    res.send("welcome bcb")
+})
+
+
+app.listen(process.env.port,async()=>{
+    try {
+        await connection
+        console.log("runing");
+        
+    } catch (error) {
+        console.log(error);
+    }
+   
+}) 
+
+// https://rose-hilarious-bat.cyclic.app/
+
+// "year": 2016,
+// "make": "audi",
+// "model": "x4",
+// "kms": "20000 km",
+// "transmission": "petrol",
+// "exteriorcolor": "red",
+// "fuel": "petrol",
+// "price": 90080,
+// "image": "https://cdn.bigboytoyz.com/new-version/assets/images/no-image-3.png"
